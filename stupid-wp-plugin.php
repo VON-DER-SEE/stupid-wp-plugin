6d9aafa4cc7fa531dc4cafd1bf27dee7b64611c5
Fabian Kielmann <fabian.kielmann@gmx.de>
2023-03-09 08:54:23 +0100

    first commit

---
+++ stupid-wp-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: My Awesome Plugin
+ * Plugin URI: https://example.com/
+ * Description: A brief description of what your plugin does.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Author URI: https://yourwebsite.com/
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// Define any necessary constants or global variables here.
+
+// Add any necessary actions or filters here.
+
+// Define any necessary functions or classes here.
+
+// Initialize your plugin here.
